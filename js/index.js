9985ce29804c0b3bd278b9a1c490732912e65761
jeferson <jeferson.contato.df@gmail.com>
2023-10-04 20:14:52 -0300

    SPA Universe and Routes in Js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import {Router} from './router.js'
+
+const router = new Router()
+
+router.add("/" , "pages/home.html")
+router.add("/universe" , "pages/universe.html")
+router.add("/exploration" , "pages/exploration.html")
+router.add(404 , "pages/404.html")
+
+router.handle()
+window.onpopstate = () => router.handle() // para navegar entre as setas no nevegador
+window.rounte = () => router.rounte() // para executar a rota em toda a janela
+
